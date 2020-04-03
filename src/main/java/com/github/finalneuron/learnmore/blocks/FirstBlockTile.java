8087ef99c879a0d2c4b25969ce708c282ae21e2c
jim <dijpak@gmail.com>
2020-04-03 16:38:56 -0500

    Added inventory to FirstBlockTile.java by adding capability through applying CompoundNBT tag.

--- src/main/java/com/github/finalneuron/learnmore/blocks/FirstBlockTile.java
+++ src/main/java/com/github/finalneuron/learnmore/blocks/FirstBlockTile.java
@@ -1,10 +1,21 @@
 package com.github.finalneuron.learnmore.blocks;
 
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
+
+    private ItemStackHandler handler;
 
     public FirstBlockTile() {
         super(ModBlocks.FIRSTBLOCK_TILE);
@@ -13,8 +24,35 @@ public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
 
     @Override
     public void tick() {
-        if (world.isRemote) {
-            System.out.println("FirstBlockTile.tick");
+    }
+
+    @Override
+    public void read(CompoundNBT tag) {
+        CompoundNBT invTag = tag.getCompound("inv");
+        getHandler().deserializeNBT(invTag);
+        super.read(tag);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT tag) {
+        CompoundNBT compound = getHandler().serializeNBT();
+        tag.put("inv", compound);
+        return super.write(tag);
+    }
+
+    private ItemStackHandler getHandler() {
+        if (handler == null){
+            handler = new ItemStackHandler(1);
         }
+        return handler;
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return LazyOptional.of(() -> (T) getHandler();
+        }
+        return super.getCapability(cap, side);
     }
 }
